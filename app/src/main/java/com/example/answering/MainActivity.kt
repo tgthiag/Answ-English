c1a9e60bae80acd558d168fff3d7993aa5c20348
tgthiag <tgthiag@gmail.com>
2022-03-06 01:47:41 -0300

    BD_SQL para persistência de dados, layouts responsivos p/ telas menores, criando tela de configuração, sistema de moedas.

--- app/src/main/java/com/example/answering/MainActivity.kt
+++ app/src/main/java/com/example/answering/MainActivity.kt
@@ -1,9 +1,14 @@
-package com.example.answering.activities
+package com.example.answering
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.answering.activities.SettingsScreen
+import com.example.answering.activities.beginner
+
+
 import com.example.answering.databinding.ActivityMainBinding
+
 
 private lateinit var binding : ActivityMainBinding
 class MainActivity : AppCompatActivity() {
@@ -11,11 +16,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
 
 
         binding.btAboutYou.setOnClickListener {
-            val intent = Intent(this,beginner::class.java)
+            val intent = Intent(this, beginner::class.java)
             intent.putExtra("level", "basic")
             startActivity(intent)
         }
@@ -28,6 +34,19 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,beginner::class.java)
             intent.putExtra("level", "advanced")
             startActivity(intent)
+        }
+        binding.btBegInterm.setOnClickListener {
+            val intent = Intent(this,beginner::class.java)
+            intent.putExtra("level", "begInterm")
+            startActivity(intent)
+        }
+        binding.allQuestions .setOnClickListener {
+            val intent = Intent(this,beginner::class.java)
+            intent.putExtra("level", "allquestions")
+            startActivity(intent)
+        }
+        binding.btSettings.setOnClickListener {
+            startActivity(Intent(this,SettingsScreen::class.java))
         }
     }
 }
--- app/src/main/java/com/example/answering/activities/beginner.kt
+++ app/src/main/java/com/example/answering/activities/beginner.kt
@@ -1,79 +1,140 @@
 package com.example.answering.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
-import android.util.Log
-import android.widget.Toast
-import com.example.answering.R
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.answering.dados.Questions
+import com.example.answering.dados.TABLE_NAME
+import com.example.answering.dados.localSqlDatabase
 import com.example.answering.databinding.BeginnerBinding
-import com.example.answering.funcoes.Admob
-import com.example.answering.funcoes.Question
-import com.example.answering.funcoes.mTTS
-import com.example.answering.questions.Questions
-import java.util.*
+import com.example.answering.funcoes.rewardedAd
+import com.example.answering.funcoes.textToSpeak
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 private lateinit var binding: BeginnerBinding
 class beginner : AppCompatActivity() {
+    private var interstitialAd: InterstitialAd? = null
+    private val bannerAd : AdView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = BeginnerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        supportActionBar?.hide()
+        supportActionBar?.hide()
 
-        //Call AD FROM ADMOB
-        Admob().showAd(this, binding.adView, binding.adView2)
+        //==========DATABASE-MYSQL==============
+        val mainClass = this
+        val classdb = localSqlDatabase(mainClass)
+        val db = classdb.writableDatabase
+        classdb.initializeRow()
 
-        //Chamando as perguntas beginner
+        //====CARREGANDO MOEDAS==========
+
+        loadCoins(this,db)
+
+        //===========AD-MOB=========
+        MobileAds.initialize(this) {}
+        loadInterstitial()
+        loadBanner()
+        rewardedAd().loadReward(this)
+
+
+        //========BT-AD-REWARD========
+        binding.btReward.setOnClickListener {
+            rewardedAd().showAd(this,db)
+            loadCoins(this,db)
+        }
+
+
+
+
+        //===========CHAMANDO PERGUNTAS==============
         var questions = Questions(this)
         var selectedLevel : Array<List<String>>
         var level:String = intent.getStringExtra("level").toString()
-        Toast.makeText(this,"level $level selected",Toast.LENGTH_SHORT).show()
         when{
             level == "basic" -> selectedLevel = questions.Basic
             level == "intermediate" -> selectedLevel = questions.Intermediate
             level == "advanced" -> selectedLevel = questions.Advanced
+            level == "begInterm" -> selectedLevel = questions.begInterm
+            level == "allquestions" -> selectedLevel = questions.allLevels
             else ->  selectedLevel = questions.Basic
         }
+        //EMBARALHANDO PERGUNTAS
         selectedLevel.shuffle()
 
-
-        var posicao = 0
-        Question().click(applicationContext,binding.cxTexto, binding.cxHint, selectedLevel, binding.checkDicas,posicao)
-
-        binding.btFront.setOnClickListener {
-            if (posicao < selectedLevel.size -1){ posicao = posicao + 1} else{ posicao = 0}
-            Question().click(applicationContext,binding.cxTexto, binding.cxHint, selectedLevel, binding.checkDicas,posicao)
-        }
-
-        binding.btBack.setOnClickListener {
-            if (posicao != 0){ posicao = posicao - 1} else{ posicao = 0}
-            Question().click(applicationContext,binding.cxTexto,binding.cxHint, selectedLevel, binding.checkDicas,posicao)
-        }
-
-        binding.playquestion.setOnClickListener {
-            mTTS = TextToSpeech(applicationContext,TextToSpeech.OnInitListener { status ->
-                if (status != TextToSpeech.ERROR){
-                    mTTS.setLanguage(Locale.US)
-                    mTTS.speak(binding.cxTexto.text,TextToSpeech.QUEUE_FLUSH,null,null)
-                }
-            })
-        }
-
-        binding.playhint.setOnClickListener {
-            mTTS = TextToSpeech(applicationContext,TextToSpeech.OnInitListener { status ->
-                if (status != TextToSpeech.ERROR){
-                    if (binding.cxHint.text != "****") {
-                        mTTS.setLanguage(Locale.US)
-                        mTTS.speak(binding.cxHint.text.toString(), TextToSpeech.QUEUE_FLUSH, null, null)
-                    }else{
-                        mTTS.defaultVoice
-                        mTTS.speak(this.getString(R.string.nohintsavailable), TextToSpeech.QUEUE_FLUSH, null, null)
-                        Toast.makeText(this,this.getString(R.string.nohintsavailable),Toast.LENGTH_SHORT).show()}
-                }
-            })
-        }
+        //==========INICIANDO FUNÇÕES DA TELA DE PERGUNTAS===============
+        textToSpeak.TTS(this,selectedLevel, binding.cxTexto, binding.cxHint,applicationContext, binding.playquestion,
+            binding.playhint, binding.checkDicas, binding.btFront, binding.btBack, binding.btAutomatic,db)
 
     }
+    public override fun onPause() {
+        binding.adView.pause()
+        binding.adView2?.pause()
+        super.onPause()
+    }
+
+    // Called when returning to the activity
+    public override fun onResume() {
+        super.onResume()
+        binding.adView.resume()
+        binding.adView2?.resume()
+    }
+
+    // Called before the activity is destroyed
+    public override fun onDestroy() {
+        binding.adView.destroy()
+        binding.adView2?.destroy()
+        super.onDestroy()
+    }
+
+    fun loadCoins(ctx: Context, db : SQLiteDatabase): Int {
+        val selectQuery = "SELECT * FROM $TABLE_NAME;"
+        val cursor = db.rawQuery(selectQuery, null)
+        cursor.moveToFirst()
+        var result = cursor.getString(2).toInt()
+        binding.txtMoedas.text = result.toString()
+        cursor.close()
+        return result
+
+    }
+
+    private fun loadBanner() {
+        val adRequest = AdRequest.Builder().build()
+        binding.adView2?.loadAd(adRequest)
+        binding.adView.loadAd(adRequest)
+//      real banner  ca-app-pub-2884509228034182/8623568236
+//     teste   ca-app-pub-3940256099942544/6300978111
+    }
+//real: ca-app-pub-2884509228034182/7941879672
+    //teste: ca-app-pub-3940256099942544/8691691433
+    private fun loadInterstitial() {
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/8691691433",adRequest,object :InterstitialAdLoadCallback(){
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                super.onAdFailedToLoad(p0)
+                interstitialAd = null
+            }
+
+            override fun onAdLoaded(p0: InterstitialAd) {
+                super.onAdLoaded(p0)
+                interstitialAd = p0
+            }
+        }
+
+
+        )
+    }
+
+    fun showInterstitial(view : View){
+        interstitialAd?.show(this)
+    }
+
 }
